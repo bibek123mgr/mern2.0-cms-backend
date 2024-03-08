2e2420eda794c8ddc988321290aea494d9ce7455
bibek <bibekmagar746@gmail.com>
2024-03-08 19:05:17 +0545

    make schedule

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const axios = require('axios')
+const schedule = require('node-schedule');
 const cors = require('cors');
 const dotenv = require('dotenv')
 const connectTodatabase = require('./db')
@@ -26,6 +28,14 @@ app.use(express.static("storage/"))
 app.listen(port, () => {
     console.log(`app is listening at https://mern2-0-cms-backend.onrender.com/`)
 })
+
+schedule.scheduleJob("*/12 * * * *", async () => {
+    const response = await axios.get(`https://mern2-0-cms-backend.onrender.com/`)
+    if (response.status === 200) {
+        console.log(' i am active')
+    }
+});
+
 
 
 
